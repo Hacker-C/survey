0a2e2f4d11dbe0092b8c519967cde33817f9595b
Hacker-C <1507559148@qq.com>
2023-04-23 00:24:01 +0800

    💄 style: 404 页面

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,7 +1,15 @@
+import { Button, Typography } from 'antd'
+import { Link } from 'react-router-dom'
+import NotFoundSvg from '~/assets/svgs/404.svg'
+
 export function NotFound() {
   return (
-    <>
-      <h2>404 not found</h2>
-    </>
+    <div className='flex flex-col items-center mt30'>
+      <img src={NotFoundSvg} alt={'NotFoundSvg'} width={'280'} />
+      <Typography.Text className='my5'>抱歉, 您访问的页面不存在</Typography.Text>
+      <Link to={'/'}>
+        <Button type="primary" size={'large'}>回到首页</Button>
+      </Link>
+    </div>
   )
 }
