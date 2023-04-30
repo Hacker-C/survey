fbe1c98f53d0b8eadad266d3998a76ea63955a2f
Hacker-C <1507559148@qq.com>
2023-04-30 12:34:38 +0800

    ✨ feat: 优化 404 跳转

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,15 +1,14 @@
 import { Button, Typography } from 'antd'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import NotFoundSvg from '~/assets/svgs/404.svg'
 
 export function NotFound() {
+  const nav = useNavigate()
   return (
     <div className='flex flex-col items-center mt30'>
       <img src={NotFoundSvg} alt={'NotFoundSvg'} width={'280'} />
       <Typography.Text className='my5'>抱歉, 您访问的页面不存在</Typography.Text>
-      <Link to={'/'}>
-        <Button type="primary" size={'large'}>回到首页</Button>
-      </Link>
+      <Button type="primary" size={'large'} onClick={() => nav(-1)}>返回上一页</Button>
     </div>
   )
 }
